915466424dfe4ee128e65339b24488047374d5d0
Ian Davies <mr.daviesian@gmail.com>
2021-01-25 19:07:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
-  new_hash = [
+  new_hash = {
     :railroads => {}
-  ]
-  base_hash
+  }
 end
+  base_hash
 
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
